31f23d55dc38c6f3b70680bbf60f8835c7841a2f
Muhammad Ali <ali@regen.pk>
2024-10-11 13:10:18 +0500

    connectivityOption fixed

--- models/connectivityOption.js
+++ models/connectivityOption.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ConnectivitySchema = new mongoose.Schema({
-  option: { type: String, required: true, enum: ['WiFi + Cellular', 'WiFi Only'] },
+  option: { type: String, required: true, enum: ['WiFi + Cellular', 'WiFi Only', 'GPS + Cellular', 'GPS Only'] },
   deductionPercentage: { type: Number, default: 0 },
       deviceType: {
     type: String,
