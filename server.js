c9e9c8e0d4fa02e815b9a024f37ccb8a013614b6
Muhammad Ali <ali@regen.pk>
2024-11-21 14:05:02 +0500

    reworked CORS

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 // Routes
